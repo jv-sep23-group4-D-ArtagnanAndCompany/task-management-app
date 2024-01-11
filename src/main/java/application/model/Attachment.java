0bc14f11649fcb9d94dacc3f070f8eab6edb7e34
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2024-01-11 11:59:14 +0200

    fixed mvn checkstyle mistakes

--- src/main/java/application/model/Attachment.java
+++ src/main/java/application/model/Attachment.java
@@ -1,12 +1,18 @@
 package application.model;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.Data;
 import lombok.experimental.Accessors;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
--- src/main/java/application/model/Task.java
+++ src/main/java/application/model/Task.java
@@ -1,7 +1,15 @@
 package application.model;
 
-import jakarta.persistence.*;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import java.time.LocalDate;
 import lombok.Data;
 import lombok.experimental.Accessors;
