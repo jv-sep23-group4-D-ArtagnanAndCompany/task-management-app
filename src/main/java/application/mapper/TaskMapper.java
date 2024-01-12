ee7261927613a921d9bb55c65e33ef4d6cb1be91
AgroFix <colalexm@gmail.com>
2024-01-12 11:59:20 +0200

    Fixed mapper

--- src/main/java/application/mapper/TaskMapper.java
+++ src/main/java/application/mapper/TaskMapper.java
@@ -5,10 +5,22 @@ import application.dto.task.TaskResponseDto;
 import application.model.Task;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 @Mapper(config = MapperConfig.class)
 public interface TaskMapper {
     @Mapping(target = "projectId", source = "project.id")
-    @Mapping(target = "assigneeId", source = "id")
+    @Mapping(target = "assigneeId", source = "assignee.id")
+    @Mapping(target = "status", source = "status", qualifiedByName = "getStatusFromEnum")
+    @Mapping(target = "priority", source = "priority", qualifiedByName = "getPriorityFromEnum")
     TaskResponseDto toDto(Task task);
+
+    @Named("getStatusFromEnum")
+    default String getStatusFromEnum(Task.Status status) {
+        return status.toString();
+    }
+    @Named("getPriorityFromEnum")
+    default String getPriorityFromEnum(Task.Priority priority) {
+        return priority.toString();
+    }
 }
