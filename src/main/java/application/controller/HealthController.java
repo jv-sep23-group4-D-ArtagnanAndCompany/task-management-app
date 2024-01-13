175ed0e9cecde0b6e00255e746c097e4953effc0
Nataliia <gzodchie@gmail.com>
2024-01-13 17:36:20 +0200

    [Test API] - added tests for health controller and health service #25

--- src/main/java/application/controller/HealthController.java
+++ src/main/java/application/controller/HealthController.java
@@ -4,11 +4,14 @@ import application.dto.health.HealthResponseDto;
 import application.service.HealthService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@ResponseStatus(HttpStatus.OK)
 @RequiredArgsConstructor
 @RequestMapping("/api/health")
 public class HealthController {
